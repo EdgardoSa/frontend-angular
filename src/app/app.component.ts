35a67e14d153e3415c0aba73f3c7a4fad8273c31
Edgardo Sanabria <edgardo.sanabria@valmar.com.pe>
2019-04-08 20:54:43 -0500

    Se implementaron los cards.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ViewContainerData } from '@angular/core/src/view';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,116 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend-angular';
+
+  //
+
+  nombreServicio: string;
+  descripcionServicio: string;
+
+  //
+
+  mostrarServicios: services[];
+  servicios: services[] = [{
+                      title: "Electricidad",
+                      content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit,",
+                      tag: "Hogar"
+
+                    },
+                    {
+                      title: "Auxilio mecánico",
+                      content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit,",
+                      tag: "Autos"
+
+                    },
+                    {
+                      title: "Chofer reemplazo",
+                      content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit,",
+                      tag: "Autos"
+
+                    },
+                    {
+                      title: "Médico a domicilio",
+                      content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit,",
+                      tag: "Salud"
+
+                    },
+                    {
+                      title: "Ambulancia",
+                      content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit,",
+                      tag: "Salud"
+
+                    },
+                    {
+                      title: "Gasfitero",
+                      content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit,",
+                      tag: "Hogar"
+
+                    }];
+
+
+    ngOnInit(){
+
+      //inicializamos los servicios
+      this.mostrarServicios = this.servicios;
+
+    }
+           
+    /**
+     * 
+     * Metodo que filtra los servicios segun un tag
+     * 
+     * @param tag tag por el cual buscar
+     *
+     */
+    filtrarServicios(tag): void{
+
+      console.log("TAG: ",tag);
+
+      if(tag === "Todos"){
+
+        this.mostrarServicios = this.servicios;
+
+      }else{
+
+        this.mostrarServicios = this.servicios.filter((element)=>{
+          return element.tag === tag;
+        });
+
+      }
+
+    }
+
+    /**
+     * 
+     * Metodo que guarda los cambios
+     * 
+     */
+    grabar(){
+      this.servicios.push({
+                              title: this.nombreServicio,
+                              content: this.descripcionServicio,
+                              tag: "Todos"
+
+                            });
+    }
+
+    /**
+     * 
+     * Metodo que cancela los cambios
+     * 
+     */
+    cancelar(){
+      this.nombreServicio = "";
+      this.descripcionServicio = "";
+    }
+
+}
+
+export interface services{
+
+    title: string,
+    content: string,
+    tag: string
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,7 +17,14 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   ],
   imports: [
     BrowserModule,
-    MatToolbarModule
+    MatGridListModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
